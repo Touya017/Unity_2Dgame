f15ccda3a1dd82eefa25ac6ae2f3a14912e06cf9
unknown <>
2016-07-01 21:51:22 +0900

    アニメーション編集

---
+++ ActionGameVer5.3.5/Assets/Script/EnemyColliderCheck.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyColliderCheck : MonoBehaviour {
+
+    // 敵の攻撃力
+    public int eAttack = 1;
+
+    // プレイヤーコンポーネント格納
+    GameObject player;
+
+    // プレイヤーコンポーネント記憶
+    void Awake()
+    {
+        player = GameObject.Find("Toko");
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Uniがプレイヤーに触れた時の処理
+    void OnCollisionEnter2D(Collision2D P_other)
+    {
+        if (P_other.gameObject.tag == "Player")
+        {
+            player.GetComponent<Rigidbody2D>().AddForce(new Vector2(-100.0f * (player.transform.localScale.x / 1.4f), 400.0f));
+            player.GetComponent<Toko>().hp = player.GetComponent<Toko>().hp - eAttack;
+            Debug.Log(player.GetComponent<Toko>().hp);
+        }
+    }
+}
