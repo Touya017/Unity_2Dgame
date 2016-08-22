fe94c4027a298ec388a48fd1b495f80499e5f51f
unknown <>
2016-08-22 20:23:10 +0900

    0822上げ

---
+++ ActionGameVer5.3.5_2/Assets/Sprict/EnemySprite.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySprite : MonoBehaviour
+{
+    EnemyMain enemyMain;
+
+    void Awake()
+    {
+        // EnemyMainの取得
+        enemyMain = GetComponentInParent<EnemyMain>();
+    }
+
+    void OnWillRenderObject()
+    {
+        if (Camera.current.tag == "MainCamera")
+        {
+            // 処理
+            enemyMain.cameraEnabled = true;
+        }
+    }
+}
