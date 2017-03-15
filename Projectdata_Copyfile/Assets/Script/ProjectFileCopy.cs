88f64e47c3e1698851cd644d1c890697fce73787
Tsubasa Sato <touya-fei1770@kjc.biglobe.ne.jp>
2017-03-16 01:18:53 +0900

    ProjectFileCopy作成

---
+++ Projectdata_Copyfile/Assets/Script/ProjectFileCopy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProjectFileCopy : MonoBehaviour {
+
+    UpdateFileManager Upfile;
+    ChangeFileInputManager Changefile;
+    string UpdateFileName;
+    string ChangeFileName;
+
+	// Use this for initialization
+	void Start () {
+        Upfile = GetComponent<UpdateFileManager>();
+        Changefile = GetComponent<ChangeFileInputManager>();
+    }
+	
+	public void Update () {
+
+        if (Upfile == null && Changefile == null)
+        {
+            return;
+        }
+        else
+        {
+            // InputFieldに入力されたファイルパスの文字列を格納
+            UpdateFileName = Upfile.GetComponentInParent<InputField>().text;
+            ChangeFileName = Changefile.GetComponentInParent<InputField>().text;
+            Debug.Log(UpdateFileName);
+            Debug.Log(ChangeFileName);
+
+            //第3引数にtruを指定すると、コピー先が存在している場合は上書きする
+            //上書きするファイルが読み取り専用などで上書きできない場合は、
+            //  UnauthorizedAccessExceptionが発生
+            System.IO.File.Copy(@"UpdatefileName", @"ChangeFileName", true);
+        }
+        return;
+    }
+}
---
+++ Projectdata_Copyfile/Assets/Script/UpdateFileManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateFileManager : MonoBehaviour {
+
+    InputField inputField;
+
+    // InputFieldのコンポーネント取得と初期化
+    void Start()
+    {
+        inputField = GetComponent<InputField>();
+        InitInputField("", false);
+    }
+
+    // Log出力と確認用、その後初期化
+    public void InputLogger()
+    {
+        string inputValue = inputField.text;
+        Debug.Log(inputValue);
+        InitInputField("", false);
+    }
+
+    // InputFieldの初期化処理
+    void InitInputField(string initString, bool Forcus)
+    {
+        // テキスト内を初期化
+        inputField.text = initString;
+        // InputFieldをフォーカスする
+        if (Forcus == true)
+        {
+            inputField.ActivateInputField();
+        }
+        else
+        {
+            inputField.DeactivateInputField();
+        }
+    }
+}
